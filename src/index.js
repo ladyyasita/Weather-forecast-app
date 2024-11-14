aabb3d8f20925af8600b1e61e8b81a90af5e28b6
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2024-11-14 05:14:49 +0100

    switched to the openweather

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 function displayWeather(response) {
   let tempElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = response.data.city;
-  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.name;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description-weather");
-  descriptionElement.innerHTML = response.data.condition.description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   let currentTimeElement = document.querySelector("#current-time");
-  let date = new Date(response.data.time * 1000);
+  let date = new Date(response.data.dt * 1000);
   currentTimeElement.innerHTML = formatDate(date);
   let iconTempElement = document.querySelector("#temp-icon");
-  iconTempElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temp-icon"/>`;
+  iconTempElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png " class="temp-icon"/>`;
+  let realFeel = response.data.main.feels_like;
   let realFeelElement = document.querySelector("#realfeel");
-  let realFeel = data.feels_like.current;
   realFeelElement.innerHTML = `${Math.round(realFeel)}°C`;
 }
 
 function search(city) {
-  let apiKey = "c3a451d0adt46fobb2b9a77755f49315";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayWeather);
 }
 function handleSubmit(event) {
